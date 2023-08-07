76c8a8ce17b5cbaa351daeb16101cd23328daf77
stefanm361 <75280942+stefanm361@users.noreply.github.com>
2023-08-07 13:47:51 -0700

    Update cli.py

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -35,7 +35,6 @@ def main():
     srt_only: bool = args.pop("srt_only")
     language: str = args.pop("language")
     
-    
     os.makedirs(output_dir, exist_ok=True)
 
     if model_name.endswith(".en"):
@@ -64,7 +63,7 @@ def main():
         audio = video.audio
 
         ffmpeg.concat(
-            video.filter('subtitles', srt_path, force_style="Fontsize=22,Fontname=Helvetica-Bold,PrimaryColour=&H00FFFF&", audio, v=1, a=1
+            video.filter('subtitles', srt_path, force_style="Fontsize=22,Fontname=Helvetica-Bold,PrimaryColour=&H00FFFF&"), audio, v=1, a=1
         ).output(out_path).run(quiet=True, overwrite_output=True)
 
         print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
