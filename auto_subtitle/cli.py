d43046add59a36de35a6f556a6dfc7823fe53716
stefanm361 <75280942+stefanm361@users.noreply.github.com>
2023-08-07 15:35:30 -0700

    Update cli.py

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -59,7 +59,7 @@ def main():
         return
 
     if sub_loc == "below":
-        font_size = "12"
+        font_size = 12
 
     print(font_size)
     
@@ -72,7 +72,7 @@ def main():
         audio = video.audio
 
         ffmpeg.concat(
-            video.filter('subtitles', srt_path, force_style="Fontsize={},Fontname=Helvetica-Bold,PrimaryColour=&H00FFFF&,Alignment=0, MarginV=50".format(font_size)), audio, v=1, a=1
+            video.filter('subtitles', srt_path, force_style="Fontsize={},Fontname=Helvetica-Bold,PrimaryColour=&H00FFFF&,Alignment=2, MarginV=500".format(font_size)), audio, v=1, a=1
         ).output(out_path).run(quiet=True, overwrite_output=True)
 
         print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
